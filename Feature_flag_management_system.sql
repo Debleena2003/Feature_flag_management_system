4c7109846507f00f2eac697589cd3f4696b58598
Debleena2003 <137258272+Debleena2003@users.noreply.github.com>
2025-06-22 12:51:13 +0530

    update

--- Feature_flag_management_system.sql
+++ Feature_flag_management_system.sql
@@ -1,14 +1,44 @@
-/*create table user_group_membership(user_id int, group_id int, primary key(user_id,group_id),
+#feature_flags
+ create table feature_flags (
+    flag_id int auto_increment primary key,
+    flag_name varchar(100) unique not null,
+    description text,
+    is_enabled boolean default false,
+    expires_at datetime,
+    created_at datetime default current_timestamp,
+    updated_at datetime default current_timestamp on update current_timestamp
+);
+#Environments
+create table environments (
+    env_id int auto_increment primary key,
+    env_name varchar(50) unique not null
+);
+#Users
+create table users (
+    user_id int auto_increment primary key,
+    username varchar(100) unique not null
+);
+#User_Groups
+create table user_groups (
+    group_id int auto_increment primary key,
+    group_name varchar(100) unique not null
+);
+
+#user_group_membership
+ create table user_group_membership(user_id int, group_id int, primary key(user_id,group_id),
 foreign key(user_id) references users(user_id),
-foreign key (group_id) references user_groups(group_id)); */
+foreign key (group_id) references user_groups(group_id)); 
+
 #select * from user_group_membership;
-/*create table flag_assignments(assignment_id int auto_increment primary key, flag_id int, env_id int, user_id int default null,
+
+--flag_assignments
+create table flag_assignments(assignment_id int auto_increment primary key, flag_id int, env_id int, user_id int default null,
  group_id int default null, is_enabled boolean default true, assigned_at datetime default current_timestamp,
  expires_at datetime default null,
  foreign key(flag_id)references feature_flags(flag_id),
 foreign key(env_id)references environments(env_id),
 foreign key(user_id)references users(user_id),
-foreign key(group_id)references user_groups(group_id)); */
+foreign key(group_id)references user_groups(group_id)); 
 
 /*create table flag_dependencies(parent_flag_id int , child_flag_id int, primary key (parent_flag_id, child_falg_id),
 foreign key (parent_flag_id) references feature_flags(flag_id),
@@ -20,18 +50,18 @@ flag_id int, actor varchar(100) not null,
 change_time datetime default current_timestamp, description text,
 foreign key (flag_id) references feature_flags(flag_id)); */
 
-#SQL queries and operations
-#update feature_flags set is_enabled=true, updated_at=NOW() where flag_name='new_dashboard';
+--SQL queries and operations
+--update feature_flags set is_enabled=true, updated_at=NOW() where flag_name='new_dashboard';
 /*insert into feature_flags (flag_name, description, is_enabled)
 values ('new_dashboard', 'Enable new dashboard UI', TRUE);*/
 
-#insert into audit_logs(action_type, flag_id,actor, description) values('update', 1, 'admin_user', 'Enabled new_dashboard flag');
-#SELECT * FROM feature_flags WHERE flag_id = 1;
+--insert into audit_logs(action_type, flag_id,actor, description) values('update', 1, 'admin_user', 'Enabled new_dashboard flag');
+--SELECT * FROM feature_flags WHERE flag_id = 1;
 
-#Assign flag to user or group for a specific environment
-#insert into users(user_id, username) values (101, 'test_user');
-#insert into flag_assignments(flag_id,env_id, group_id, is_enabled) values (1,2,5,true);
-#insert into user_groups (group_id, group_name) values (5, 'qa_team');
+--Assign flag to user or group for a specific environment
+--insert into users(user_id, username) values (101, 'test_user');
+--insert into flag_assignments(flag_id,env_id, group_id, is_enabled) values (1,2,5,true);
+--insert into user_groups (group_id, group_name) values (5, 'qa_team');
 
 /*select distinct ff.flag_name
 from feature_flags ff
@@ -46,26 +76,26 @@ and ff.is_enabled = true
 and (ff.expires_at is null or ff.expires_at > now())
 and (fa.expires_at is null or fa.expires_at > now());*/
 
-/*create table flag_dependencies (
+create table flag_dependencies (
     parent_flag_id int,
     child_flag_id int,
     primary key (parent_flag_id, child_flag_id),
     foreign key (parent_flag_id) references feature_flags(flag_id),
-    foreign key (child_flag_id) references feature_flags(flag_id));*/
+    foreign key (child_flag_id) references feature_flags(flag_id));
     
- #insert into feature_flags (flag_name, description, is_enabled) values ('dependent_feature', 'This feature depends on another', false);
+ --insert into feature_flags (flag_name, description, is_enabled) values ('dependent_feature', 'This feature depends on another', false);
 
 
 
-/*select pf.flag_name as parent_flag, cf.flag_name as dependent_flag
+select pf.flag_name as parent_flag, cf.flag_name as dependent_flag
 from flag_dependencies fd
 join feature_flags pf on pf.flag_id = fd.parent_flag_id
 join feature_flags cf on cf.flag_id = fd.child_flag_id
 where cf.flag_name = 'dependent_feature'
-and pf.is_enabled = false;*/
+and pf.is_enabled = false;
 
-# creating triggers,views, procedures
-/*delimiter //
+--creating triggers,views, procedures
+delimiter //
 
 -- trigger
 create trigger trg_update_flag
@@ -85,11 +115,11 @@ begin
   insert into audit_logs (action_type, flag_id, actor, description)
   values ('update', f_id, actor_name, 'Flag enabled');
 end;//
-delimiter ;*/
+delimiter ;
 
 
-#use feature_flag_db;
-#call enable_flag(1,'admin');
+use feature_flag_db;
+call enable_flag(1,'admin');
 select * from audit_logs;
 
 
